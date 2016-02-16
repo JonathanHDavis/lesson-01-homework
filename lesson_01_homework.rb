6784ca9ade4128cdcaf6bf93b01895d5aa969ce8
Jonathan Davis <jonathanherscheldavis@gmail.com>
2016-02-16 11:42:59 -0500

    Completed homework

--- lesson_01_homework.rb
+++ lesson_01_homework.rb
@@ -1,32 +1,41 @@
 # Code Reading
 
-def full_name(first_name, last_name, title)
-  # Example comment line
+ # defines what categories each string will come from
+# def full_name(first_name, last_name, title)
 
-  name = nil
+# #shows that at first we are not placing anything in for name
+#   name = nil
 
-  if title && first_name && last_name
-    name = title + " " + first_name + " " + last_name
-  elsif title && last_name
-    name = title + " " + last_name
-  elsif first_name && last_name
-    name = first_name + " " + last_name
-  elsif first_name
-    name = first_name
-  else
-    raise "Oh no, that doesn't look like a name"
-  end
+# #if all three are present do title then first then last
+#   if title && first_name && last_name
+#     name = title + " " + first_name + " " + last_name
+#   #if first missing, title then last, with a space
+#   elsif title && last_name
+#     name = title + " " + last_name
+#   #if title missing, first then last
+#   elsif first_name && last_name
+#     name = first_name + " " + last_name
+#   #if only first name is in, just puts the first name
+#   elsif first_name
+#     name = first_name
+#   #if anything else is put in, puts the "oh no" sentence
+#   else
+#     "Oh no, that doesn't look like a name"
+#   end
+#   #tells the terminal to return whatever is put in the puts
+#   return name
+# end
 
-  return name
-end
+# #populates what will be putsd
+# puts full_name("Jonathan", "Davis", "Mr.")
 
 # Fix Broken Code
 
-def full_name(f, l)
-  puts f + l
-end
+# def full_name(f, l)
+#   puts f + " " + l
+# end 
 
-puts full_nam("bubba", "chuck")
+# puts full_name("bubba", "chuck")
 
 
 # Coding
@@ -34,13 +43,31 @@ puts full_nam("bubba", "chuck")
 
 ## Enter your code for your 'add' method below
 
+# def add (num_1, num_2)
 
+#   num_1 + num_2
+
+# end
+
+# puts add(5, 9)
 
 
 ## Enter the code for you 'join_strings' method below
 
+# def join_strings(word_1, word_2)
+#   puts word_1 + " " + word_2
+# end 
 
-
-
+# puts join_strings("Howdy", "there!")
 
 ## Enter your 'old_enough_to_vote?' method below
+
+# def old_enough_to_vote?(year_born)
+#   if year_born <= 1998
+#     "Yes"
+#   else
+#     "No"
+#   end
+# end
+
+# puts old_enough_to_vote?(1998)
